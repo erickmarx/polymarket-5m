cc98922445a03d014fd731dc30beb27e83c00029
Erick Marx <erick.domingos@ageon.com.br>
2026-04-15 15:45:33 -0300

    chore: fix unused variable warning

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,7 @@ const exampleStrategy: OrderStrategy = {
     // Exemplo: só entra se tivermos histórico completo e o preço caiu no último candle
     if (history.length < 2) return state.bestAskUp > 0 && state.bestAskUp < 0.45;
     const lastCandle = history[history.length - 1];
-    // logger.log(lastCandle.close, lastCandle.open)
-    return state.bestAskUp > 0 && state.bestAskUp < 0.45 
+    return state.bestAskUp > 0 && state.bestAskUp < 0.45 && lastCandle.close < lastCandle.open;
   },
   getOrderPayload(state: MarketState, _history: Candle[]) {
     return {
